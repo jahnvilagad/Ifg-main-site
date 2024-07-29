8fb32e7ac387026ba6fa466c58f5914c038eaacb
jahnvi <jahnvi@thewebsitefactory.london>
2024-07-29 15:35:04 +0530

    checked and solved

--- intfoundationgroup/assets/js/custom.js
+++ intfoundationgroup/assets/js/custom.js
@@ -335,3 +335,16 @@ $(document).ready(function () {
   });
 });
 
+
+
+$('.courses-scroll a[href*="#"]').on('click', function (e) {
+  e.preventDefault()
+
+  $('html, body').animate(
+    {
+      scrollTop: ($($(this).attr('href')).offset().top) - 100,
+    },
+    500,
+    'linear'
+  )
+})
